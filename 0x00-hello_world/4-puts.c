db66434bf77bc100baec257462b7484c47ff257f
Abdallah <mockcoder@protonmail.ch>
2023-02-16 09:16:02 -0800

    4 hotfix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-	char Strmy[] = "Programming is like building a multilingual puzzle";
+	char Strmy[] = "\"Programming is like building a multilingual puzzle";
 	puts(Strmy);
 	return 0;
 }
