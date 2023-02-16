4a7dd4f15b4a25f08c8c302760e75d860d148b56
Abdallah <mockcoder@protonmail.ch>
2023-02-16 09:33:30 -0800

    Added 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", Mystr);
+
+	return (0);
+
+}
