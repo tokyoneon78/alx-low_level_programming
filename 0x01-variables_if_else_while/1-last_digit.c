e43916b1ffbcfe9dd1efcf3fe79598f6e4a7a18d
Abdallah <mockcoder@protonmail.ch>
2023-02-17 18:25:03 -0800

    1-last_digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@
 
 int main(void)
 {
-	int n;
+	int n,last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -27,7 +27,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
-	else if (last < 6 && last != 0)
+	else if (last_digit < 6 && last != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
