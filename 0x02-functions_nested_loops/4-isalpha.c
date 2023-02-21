894cd8da7da046bf2e83cca6530256ba359a65b8
Abdallah <mockcoder@protonmail.ch>
2023-02-21 10:10:45 -0800

    Hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,13 @@
 
 /**
  * _isalpha - checks for lowercase
- * @c: c is an ascii character
  *
  * Return: 1 if uppercase
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'z')
 	{
 		return (1);
 	}
