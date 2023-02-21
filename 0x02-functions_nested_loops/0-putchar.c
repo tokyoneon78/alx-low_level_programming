2caea35b194f307ac2eec94632d092ce6334881e
Abdallah <mockcoder@protonmail.ch>
2023-02-21 06:09:29 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+
+/**
+ * 0-putchar - Prints _putchar
+ * 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+
+_putchar("_putchar");
+
+
+
+
+}
