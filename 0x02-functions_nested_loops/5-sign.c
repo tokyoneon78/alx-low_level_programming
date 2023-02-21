9f9792575e9ef2bf2e89222266b1a6cdbdcad7c9
Abdallah <mockcoder@protonmail.ch>
2023-02-21 13:31:30 -0800

    added 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Checking signum of number
+ *
+ * Return: 1 if positive, 0 if number is 0 and -1 if negative
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+
+
+}
