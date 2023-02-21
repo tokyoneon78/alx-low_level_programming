007f6fad928a80302603d223c56c80269ba656cf
Abdallah <mockcoder@protonmail.ch>
2023-02-21 13:48:24 -0800

    Added 6-main

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * abs - Calculates distance between number and zero in the numhberline
+ * @n: integer to calculate distance from
+ *
+ *
+ *
+ *
+ * Return: always 0
+ */
+
+int abs(int)
+{
+
+	if (n < 0)
+	{
+		n = (n * -1);
+
+	} 
+
+	_putchar(n);
+
+	return (0);
+
+
+}
