39a9d60eeaa039dc3e6de74e7f47f7c92382f5c6
Abdallah Shuaibu <Mockcoder@protonmail.ch>
2023-03-07 15:05:35 +0100

    Recursion lesson

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints the string
+ * @s: string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
